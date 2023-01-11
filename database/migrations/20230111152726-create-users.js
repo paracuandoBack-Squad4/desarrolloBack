773460f0e999096197c38b03ccb491899ad57d64
Luis <luisjavier_2705@hotmail.com>
2023-01-11 13:54:48 -0500

    refactor:add transactions

--- database/migrations/20230111152726-create-users.js
+++ database/migrations/20230111152726-create-users.js
@@ -2,45 +2,59 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      first_name: {
-        type: Sequelize.STRING
-      },
-      last_name: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      email_verified: {
-        type: Sequelize.DATEONLY
-      },
-      token: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.createTable('Users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        first_name: {
+          type: Sequelize.STRING
+        },
+        last_name: {
+          type: Sequelize.STRING
+        },
+        email: {
+          type: Sequelize.STRING
+        },
+        username: {
+          type: Sequelize.STRING
+        },
+        password: {
+          type: Sequelize.STRING
+        },
+        email_verified: {
+          type: Sequelize.DATEONLY
+        },
+        token: {
+          type: Sequelize.STRING
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }, { transaction });
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.dropTable('Users');
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
   }
 };
